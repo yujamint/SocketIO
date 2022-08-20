deba7ff6666a137314078216c7fc977d74d8dd89
yujaemin <woawoa0513@gmail.com>
2022-08-20 22:33:16 +0900

    WebRTC 이용한 Rooms, P2P 연결 추가

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -4,10 +4,16 @@ const myFace = document.getElementById("myFace");
 const muteBtn = document.getElementById("mute");
 const cameraBtn = document.getElementById("camera");
 const camerasSelect = document.getElementById("cameras");
+const call = document.getElementById("call");
+
+call.hidden = true;
 
 let myStream;
 let muted = false;
 let cameraOff = false;
+let roomName;
+let myPeerConnection;
+let myDataChannel;
 
 async function getCameras() {
   try {
@@ -51,8 +57,6 @@ async function getMedia(deviceId) {
   }
 }
 
-getMedia();
-
 function handleMuteClick() {
   myStream
     .getAudioTracks()
@@ -80,9 +84,112 @@ function handleCameraClick() {
 }
 
 function handleCameraChange() {
-  getMedia(camerasSelect.value);
+  getMedia(camerasSelect.value); // 이 코드를 통해 video의 stream이 변경됐음.
+  if (myPeerConnection) {
+    const videoTrack = myStream.getVideoTracks()[0];
+    const videoSender = myPeerConnection
+      .getSenders()
+      .find((sender) => sender.track.kind === "video");
+    videoSender.replaceTrack(videoTrack);
+  }
 }
 
 muteBtn.addEventListener("click", handleMuteClick);
 cameraBtn.addEventListener("click", handleCameraClick);
 camerasSelect.addEventListener("input", handleCameraChange);
+
+// Welcome Form (join a room)
+
+const welcome = document.getElementById("welcome");
+const welcomeForm = welcome.querySelector("form");
+
+async function initCall() {
+  welcome.hidden = true;
+  call.hidden = false;
+  await getMedia();
+  makeConnection();
+}
+
+async function handleWelcomeSubmit(event) {
+  event.preventDefault();
+  const input = welcomeForm.querySelector("input");
+  await initCall();
+  socket.emit("join_room", input.value);
+  roomName = input.value;
+  input.value = "";
+}
+
+welcomeForm.addEventListener("submit", handleWelcomeSubmit);
+
+// Socket Code
+
+socket.on("welcome", async () => {
+  // peerB가 들어왔다는 알림을 받는 peerA에서 실행
+  myDataChannel = myPeerConnection.createDataChannel("chat"); // 채널 이름 : chat
+  myDataChannel.addEventListener("meesage", console.log);
+  console.log("made data channel");
+  const offer = await myPeerConnection.createOffer();
+  myPeerConnection.setLocalDescription(offer);
+  console.log("sent the offer");
+  socket.emit("offer", offer, roomName);
+});
+
+socket.on("offer", async (offer) => {
+  // peerA의 offer를 받게 되는 peerB에서 실행
+  myPeerConnection.addEventListener("datachannel", (event) => {
+    myDataChannel = event.channel;
+    myDataChannel.addEventListener("message", console.log);
+  }); // data를 받아서 바로 console.log
+  console.log("received the offer");
+  myPeerConnection.setRemoteDescription(offer);
+  const answer = await myPeerConnection.createAnswer();
+  myPeerConnection.setLocalDescription(answer);
+  socket.emit("answer", answer, roomName);
+  console.log("sent the answer");
+});
+
+socket.on("answer", (answer) => {
+  // peerB의 answer를 받는 peerA에서 실행
+  console.log("received the answer");
+  myPeerConnection.setRemoteDescription(answer);
+});
+
+socket.on("ice", (ice) => {
+  console.log("received candidate");
+  myPeerConnection.addIceCandidate(ice);
+});
+
+//RTC code
+
+function makeConnection() {
+  myPeerConnection = new RTCPeerConnection();
+  //  STUN Server
+  //   {
+  //   iceServers: [
+  //     {
+  //       urls: [
+  //         "stun:stun.l.google.com:19302",
+  //         "stun:stun1.l.google.com:19302",
+  //         "stun:stun2.l.google.com:19302",
+  //         "stun:stun3.l.google.com:19302",
+  //         "stun:  stun4.l.google.com:19302",
+  //       ],
+  //     },
+  //   ],
+  // }
+  myPeerConnection.addEventListener("icecandidate", handleIce);
+  myPeerConnection.addEventListener("addstream", handleAddStream);
+  myStream
+    .getTracks()
+    .forEach((track) => myPeerConnection.addTrack(track, myStream));
+}
+
+function handleIce(data) {
+  console.log("sent candidate");
+  socket.emit("ice", data.candidate, roomName);
+}
+
+function handleAddStream(data) {
+  const peerFace = document.getElementById("peerFace");
+  peerFace.srcObject = data.stream; // 상대 브라우저의 stream 정보(data.stream)를 home.pug의 video#peerFace에 넣어준다.
+}
--- src/server.js
+++ src/server.js
@@ -1,6 +1,5 @@
 import http from "http";
-import { Server } from "socket.io";
-import { instrument } from "@socket.io/admin-ui";
+import SocketIO from "socket.io";
 import express from "express";
 
 const app = express();
@@ -10,84 +9,28 @@ app.set("views", __dirname + "/views"); // Express에 template이 어디 있는�
 app.use("/public", express.static(__dirname + "/public")); // 유저가 /public으로 가게되면 __dirname + "/public" 폴더를 보여준다.
 // public url을 생성해서 유저에게 파일을 공유해준다.
 
-app.get("/", (req,res) => res.render("home")); // 홈으로 이동하면 "home" 렌더링
+app.get("/", (req, res) => res.render("home")); // 홈으로 이동하면 "home" 렌더링
 // home.pug를 render 해주는 route handler
 app.get("/*", (req, res) => res.redirect("/")); // 어떤 url로 들어오든 홈으로 보낸다.
 
 const httpServer = http.createServer(app); // http 서버 생성
-const wsServer = new Server(httpServer, {
-    cors: {
-        origin: ["https://admin.socket.io"],
-        credentials: true,
-    },
-});
-instrument(wsServer, {
-    auth: false
-});
-// const wss = new WebSocket.Server({ server }) //http 서버 위에 웹소켓 서버 생성 -> http 서버와 웹소켓 서버 모두 돌릴 수 있다.
-// 동일한 포트(localhost:3000)에서 http와 ws의 request 모두 처리할 수 있다.(필수사항은 아니다)
+const wsServer = SocketIO(httpServer);
 
-function publicRooms() {
-    const {
-        sockets : {
-            adapter : {sids, rooms}
-        },
-    } = wsServer;
-    const publicRooms = [];
-    rooms.forEach((_, key) => {
-        if(sids.get(key) === undefined) {
-            publicRooms.push(key);
-        }
-    });
-    return publicRooms;    
-}
-
-function countRoom(roomName) {
-    return wsServer.sockets.adapter.rooms.get(roomName)?.size;
-}
-
-wsServer.on("connection", socket => {
-    wsServer.sockets.emit("room_change", publicRooms());
-    socket["nickname"] = "Anon";
-    socket.onAny((event) => {
-        console.log(wsServer.sockets.adapter);
-        console.log(`Socket "Event:${event}`);
-    });
-    socket.on("enter_room", (roomName , done) => {
-        socket.join(roomName);
-        done();
-        wsServer.to(roomName).emit("welcome", socket.nickname ,countRoom(roomName));
-        wsServer.sockets.emit("room_change", publicRooms());
-    });
-    socket.on("disconnecting", () => {
-        socket.rooms.forEach( (room) => socket.to(room).emit("bye", socket.nickname, countRoom(room)- 1));
-    });
-    socket.on("disconnect", () => {
-        wsServer.sockets.emit("room_change", publicRooms());
-    })
-    socket.on("new_message", (msg, room, done) => {
-        socket.to(room).emit("new_message", `${socket.nickname}: ${msg}`);
-        done();
-    });
-    socket.on("nickname", nickname => socket["nickname"] = nickname);
+wsServer.on("connection", (socket) => {
+  socket.on("join_room", (roomName) => {
+    socket.join(roomName);
+    socket.to(roomName).emit("welcome");
+  });
+  socket.on("offer", (offer, roomName) => {
+    socket.to(roomName).emit("offer", offer);
+  });
+  socket.on("answer", (answer, roomName) => {
+    socket.to(roomName).emit("answer", answer);
+  });
+  socket.on("ice", (ice, roomName) => {
+    socket.to(roomName).emit("ice", ice);
+  });
 });
 
-/* const sockets = [];
-wss.on("connection", (socket) => {
-    sockets.push(socket);
-    socket["nickname"] = "Anon";
-    console.log("Connected to Browser 🎈");
-    socket.on("close", () => console.log("Disconnected from Browser ❌"));
-    socket.on("message", (msg) => {
-        const message = JSON.parse(msg);
-        switch(message.type) {
-            case "new_message" : 
-                sockets.forEach((aSocket) => aSocket.send(`${socket.nickname} : ${message.payload}`));
-            case "nickname" : 
-                socket["nickname"] = message.payload;
-        }
-    });
-}); */
-
-const handleListen = () => console.log('Listening on http://localhost:3000');
+const handleListen = () => console.log("Listening on http://localhost:3000");
 httpServer.listen(3000, handleListen);
